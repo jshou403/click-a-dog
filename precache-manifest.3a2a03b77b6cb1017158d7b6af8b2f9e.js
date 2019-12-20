8ed265b0d01c2f6372fcf16271a95aa9f36955dd
JACK_E <jshou403@gmail.com>
2019-12-19 23:49:56 -0500

    Updates

---
+++ precache-manifest.3a2a03b77b6cb1017158d7b6af8b2f9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f05d65a9707479234f456ab90ae6e331",
+    "url": "/click-a-dog/index.html"
+  },
+  {
+    "revision": "c683ba6e437a85a341d4",
+    "url": "/click-a-dog/static/css/main.f4aab129.chunk.css"
+  },
+  {
+    "revision": "35c814bdf68b6ad80378",
+    "url": "/click-a-dog/static/js/2.8c9fb11d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/click-a-dog/static/js/2.8c9fb11d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c683ba6e437a85a341d4",
+    "url": "/click-a-dog/static/js/main.c58656ff.chunk.js"
+  },
+  {
+    "revision": "5ffcc72368824aad036f",
+    "url": "/click-a-dog/static/js/runtime-main.5c8ddbe9.js"
+  }
+]);
